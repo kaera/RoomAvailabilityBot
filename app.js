18a1b0521c6e4781019b87c40ba2d332bdf3b64f
Anastasia Novak <anastasy.d@gmail.com>
2018-06-17 20:50:15 +0200

    Add fetching availability data

--- app.js
+++ app.js
@@ -21,10 +21,57 @@ function sendMessage(res, options) {
     });
 }
 
+function fetchAvailabilityData() {
+	return axios({
+		    method: 'post',
+		    url: 'https://centrale.ffcam.fr/index.php',
+		    data: 'structure=BK_STRUCTURE:30'
+		})
+		.then(function (response) {
+			return JSON.parse(response.data.match(/globalAvailability = (.*?);/)[1]);
+		});
+}
+
+function poll(date, chatId) {
+	return fetchAvailabilityData()
+		.then(data => {
+			if (data[date] === undefined) {
+				// Date is invalid
+				// Stop polling and send message
+				axios.post(`https://api.telegram.org/bot${ tokens.botToken }/sendMessage`, {
+					chat_id: chatId,
+					text: `Unable to poll for date ${ date } as it's out of range. Please specify new date.`
+				});
+			} else if (data[date] === 0) {
+				// Continue polling
+				setTimeout(poll, 30 * 60 * 1000, date, chatId);
+			} else {
+				// Places are found
+				// Stop polling and send message
+				axios.post(`https://api.telegram.org/bot${ tokens.botToken }/sendMessage`, {
+					chat_id: chatId,
+					text: `${ data[date] } places found for date ${ date }! You can book them here: http://refugedugouter.ffcam.fr/resapublic.html.`
+				});
+			}
+		});
+}
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello, my bot!');
+});
+
+app.get('/debug', (req, res) => {
+	// poll('2017-01-01', 174442510)
+	poll('2018-09-25', 174442510)
+		.then(function () {
+			res.send({ status: 'ok'});
+		})
+		.catch(error => {
+			console.log(error);
+			res.status(500).send('Error');
+		});
 });
 
 app.post('/bot/' + tokens.webhookToken, (req, res) => {
@@ -41,12 +88,13 @@ app.post('/bot/' + tokens.webhookToken, (req, res) => {
 		}
 		pollingData[username] = date;
 		responseText += 'Starting polling availability for date ' + date;
+		poll(date, message.chat.id);
 	} else if (message.text === '/stop') {
 		responseText = 'Polling cancelled for date ' + pollingData[username];
 	} else {
-		responseText = `Hello ${message.from.first_name} ${message.from.last_name}!
-      
-      ${JSON.stringify(message, null, 2)}`;
+		responseText = `Couldn't recognize the message. Please, try again :)
+
+		${JSON.stringify(message, null, 2)}`;
 	}
 	
     const options = {
