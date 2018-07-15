3df1a70332b5e8f73a64f5c370f7c85e142c7807
Anastasia Novak <anastasy.d@gmail.com>
2018-07-15 20:55:50 +0200

    Add tokens_sample.js

---
+++ tokens_sample.js
@@ -0,0 +1,14 @@
+module.exports = {
+	botToken: 'token',
+	webhookToken: 'token',
+	urlRefuge: 'https://url.com',
+	dataRefuge: 'data',
+	db: {
+		host: 'host.sample.com',
+		name: 'name',
+		collection: 'name',
+		user: 'user',
+		pass: 'pass',
+		port: '12345'
+	}	
+};
